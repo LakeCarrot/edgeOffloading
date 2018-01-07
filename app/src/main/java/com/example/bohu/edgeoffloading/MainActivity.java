58e0645df06982b2738add5428973eef2888d756
LakeCarrot <hubocarrothubo@gmail.com>
2018-01-07 14:03:13 -0500

    finish gRPC client part logic

--- app/src/main/java/com/example/bohu/edgeoffloading/MainActivity.java
+++ app/src/main/java/com/example/bohu/edgeoffloading/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.bohu.edgeoffloading;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.concurrent.TimeUnit;
+
+import edgeOffloading.OffloadingGrpc;
+import edgeOffloading.OffloadingOuterClass;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new GrpcTask().execute();
+    }
+
+    private class GrpcTask extends AsyncTask<Void, Void, String> {
+        private ManagedChannel mChannel;
+        private String hostIP;
+        private int hostPort;
+
+        @Override
+        protected String doInBackground(Void... nothing) {
+            try {
+                // first version use static IP and port
+                hostIP = "172.28.143.136";
+                hostPort = 50051;
+                mChannel = ManagedChannelBuilder.forAddress(hostIP, hostPort)
+                        .usePlaintext(true)
+                        .build();
+                OffloadingGrpc.OffloadingBlockingStub stub = OffloadingGrpc.newBlockingStub(mChannel);
+                OffloadingOuterClass.OffloadingRequest message = OffloadingOuterClass.OffloadingRequest.newBuilder().setMessage("Who are you?").build();
+                OffloadingOuterClass.OffloadingReply reply = stub.startService(message);
+                return reply.getMessage();
+            } catch(Exception e) {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                e.printStackTrace(pw);
+                pw.flush();
+                return String.format("Failed... : %n%s", sw);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                mChannel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println(result);
+        }
     }
 }
