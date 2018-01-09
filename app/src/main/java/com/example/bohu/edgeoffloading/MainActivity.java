bbf92774ec50ea23c332166a1154836eb5d044e2
LakeCarrot <hubocarrothubo@gmail.com>
2018-01-09 15:53:44 -0500

    face recognition in progress

--- app/src/main/java/com/example/bohu/edgeoffloading/MainActivity.java
+++ app/src/main/java/com/example/bohu/edgeoffloading/MainActivity.java
@@ -10,16 +10,24 @@ import java.util.concurrent.TimeUnit;
 
 import edgeOffloading.OffloadingGrpc;
 import edgeOffloading.OffloadingOuterClass;
+import faceRecognition.FacerecognitionGrpc;
+import faceRecognition.FacerecognitionOuterClass.FaceRecognitionRequest;
+import faceRecognition.FacerecognitionOuterClass.FaceRecognitionReply;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+// face recognition related lib
+import org.opencv.
+
 public class MainActivity extends AppCompatActivity {
+    static{ System.loadLibrary("opencv_java3"); }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new GrpcTask().execute();
+        new FaceTask().execute();
     }
 
     private class GrpcTask extends AsyncTask<Void, Void, String> {
@@ -39,6 +47,47 @@ public class MainActivity extends AppCompatActivity {
                 OffloadingGrpc.OffloadingBlockingStub stub = OffloadingGrpc.newBlockingStub(mChannel);
                 OffloadingOuterClass.OffloadingRequest message = OffloadingOuterClass.OffloadingRequest.newBuilder().setMessage("Who are you?").build();
                 OffloadingOuterClass.OffloadingReply reply = stub.startService(message);
+                return reply.getMessage();
+            } catch(Exception e) {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                e.printStackTrace(pw);
+                pw.flush();
+                return String.format("Failed... : %n%s", sw);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                mChannel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println(result);
+        }
+    }
+
+    private void faceRecognition() {
+
+    }
+    private class FaceTask extends AsyncTask<Void, Void, String> {
+        private ManagedChannel mChannel;
+        private String hostIP;
+        private int hostPort;
+
+        @Override
+        protected String doInBackground(Void... nothing) {
+            try {
+                // first version use static IP and port
+                hostIP = "172.28.143.136";
+                hostPort = 50052;
+                mChannel = ManagedChannelBuilder.forAddress(hostIP, hostPort)
+                        .usePlaintext(true)
+                        .build();
+                FacerecognitionGrpc.FacerecognitionBlockingStub stub = FacerecognitionGrpc.newBlockingStub(mChannel);
+                FaceRecognitionRequest message = FaceRecognitionRequest.newBuilder().setMessage("Can I pass?").build();
+                FaceRecognitionReply reply = stub.offloading(message);
                 return reply.getMessage();
             } catch(Exception e) {
                 StringWriter sw = new StringWriter();
